ae87671d0ff956512e18dce902b3377d0b6a9896
= <=>
2024-01-10 12:39:02 +0530

    added get_chunks method

--- src/pdf_processing.py
+++ src/pdf_processing.py
@@ -1,4 +1,6 @@
 from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from constants import constant
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -7,3 +9,7 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=constant.SPLITTER_CHUNK_SIZE, chunk_overlap=constant.SPLITTER_CHUNK_OVERLAP)
+    return text_splitter.split_text(text)
